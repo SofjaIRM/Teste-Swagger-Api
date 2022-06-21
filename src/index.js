27517feb6f0547c78c8a7782661380110d0d9ddd
Ilso <ilso_1000@hotmail.com>
2022-06-21 12:25:09 -0300

    Commit all

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const ContextStrategy = require('./db/strategies/base/contextStrategy')
+const MongoDB = require('./db/strategies/mongodb')
+const Postgres = require('./db/strategies/postgres')
+
+const ContextMongo = new ContextStrategy(new MongoDB())
+ContextMongo.create()
+
+
+const ContextPostgres = new ContextStrategy(new Postgres())
+ContextPostgres.create()
